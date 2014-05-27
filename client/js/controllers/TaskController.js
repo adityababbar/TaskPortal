633b4f895bba12fe1647850dffe75c5319ea665b
adityababbar <nopass12>
2014-05-27 02:05:39 -0400

    commit check point 001

--- client/js/controllers/TaskController.js
+++ client/js/controllers/TaskController.js
@@ -4,7 +4,32 @@
 
 var taskApp = angular.module('TaskApp' ,[]);
 
+
+
 function TaskController($scope, $http){
+
+
+        // when landing on the page, get all tasks and show them
+        $http.get('/api/tasks')
+        .success(function(data) {
+            $scope.tasks = data;
+            console.log(data);
+        })
+        .error(function(data) {
+            console.log('Error: ' + data);
+        });
+
+        var addTask=function(){
+            $http.post('/api/tasks', $scope.formData)
+                .success(function(data) {
+                    $scope.formData = {}; // clear the form so our user is ready to enter another
+                    $scope.todos = data;
+                    console.log(data);
+                })
+                .error(function(data) {
+                    console.log('Error: ' + data);
+                });
+        }
 
 
 }
--- server/server.js
+++ server/server.js
@@ -6,6 +6,18 @@ var express  = require('express');
 var app      = express(); 								// create our app w/ express
 var mongoose = require('mongoose'); 					// mongoose for mongodb
 
+
+
+console.log("#### server context root: " + __dirname);
+
+
+app.configure(function() {
+    app.use(express.static(__dirname + '/../client')); 		// set the static files location /public/img will be /img for users
+    app.use(express.logger('dev')); 						// log every request to the console
+    app.use(express.bodyParser()); 							// pull information from html in POST
+    app.use(express.methodOverride()); 						// simulate DELETE and PUT
+});
+
 // configuration =================
 console.log("#### connecting to DB");
 mongoose.connect('mongodb://admin:admin123@novus.modulusmongo.net:27017/heVu5gag'); 	// connect to mongoDB database on modulus.io
@@ -31,7 +43,7 @@ console.log("#### DB model defined");
 
 // api ---------------------------------------------------------------------
 // get all tasks
-app.get('/api/task', function(req, res) {
+app.get('/api/tasks', function(req, res) {
 //    res.header("Access-Control-Allow-Origin", "http://localhost");
     // use mongoose to get all tasks in the database
     TaskDAO.find(function(err, tasks) {
@@ -40,4 +52,30 @@ app.get('/api/task', function(req, res) {
             res.send(err)
         res.json(tasks); // return all task in JSON format
     });
+});
+
+app.post('api/tasks', function(req, res){
+    Task.create({
+        desc : req.body.desc,
+        dueDate : req.body.dueDate,
+        status: req.body.status
+    }, function(err, todo) {
+        if (err)
+            res.send(err);
+
+        // get and return all the todos after you create another
+        Task.find(function(err, task) {
+            if (err)
+                res.send(err)
+            res.json(task);
+        });
+    });
+});
+
+
+
+// application -------------------------------------------------------------
+app.get('*', function(req, res) {
+    res.sendfile( '../client/index.html'); // load the single view file (angular will handle the page changes on the front-end)
+    console.log("#### Reviceved genric url, sent the page to index.html");
 });
